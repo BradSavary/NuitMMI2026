3f0f6197dc6b363990b557c1e25bf903fbb5b72b
BradSavary <brad.savary@etu.unilim.fr>
2026-01-08 11:10:47 +0100

    first commit

---
+++ app/game/page.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Game() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      Bienvenue Sur le jeu
+    </div>
+  );
+}
